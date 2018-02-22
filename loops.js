aa1dfafa8d05b971a84b56f25331798181ed26f0
Efrain Perez Jr <efrainperezjr@live.com>
2018-02-22 18:19:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,6 +6,7 @@ function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     i === 1 ? array.push("I am ${i} strange loop."): array.push("I am ${i} strange loops.")
   }
+  return array
 }
 
 function whileLoop(n) {
